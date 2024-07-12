72d9edc6aa537c3c7365939807cff5550408ab29
KalyaniPa <52074943+KalyaniPa@users.noreply.github.com>
2024-07-12 21:04:04 +0530

    Update Pages.java

--- src/main/java/helper/Pages.java
+++ src/main/java/helper/Pages.java
@@ -1,8 +1,9 @@
 package helper;
 
 public class Pages {
-    private static final String prefix="C:\\Users\\10689577\\OneDrive - LTIMindtree\\Documents\\Learning Projects\\SeleniumProject\\src\\web\\";
+    private static final String prefix="file:///" +System.getProperty("user.dir")+"\\src\\web\\";
     public static final String HOME = prefix+"index.html";
     public static final String LOANS = prefix+"loans.html";
     public static final String SAVINGS = prefix+"savings.html";
 }
+  
